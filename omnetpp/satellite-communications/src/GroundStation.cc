fa0c5de5a6a45cfc3105163fdf547fc82feb5128
Niccolò <niccolo.mule@gmail.com>
2024-12-01 13:53:35 +0100

    Add terminal scheduling logic

--- omnetpp/satellite-communications/src/GroundStation.cc
+++ omnetpp/satellite-communications/src/GroundStation.cc
@@ -9,7 +9,18 @@ void GroundStation::initialize()
 {
     satellite = getModuleByPath("^.satellite");
     numTerminals = getParentModule()->par("N");
-    acknowledgedCodingRates = 0;
+    acknowledgedCodingRates = 0; // Maybe move this to constructor?
+
+    /* Storing CR values and queues in terminal-id indexed vectors
+     *
+     * - when new CR arrives, read the sender's terminal id (using custom msg I guess)
+     * and store the CR in the proper slot
+     *
+     * - Do the same when a new packet arrives and store the packet in
+     * the proper terminal queue
+     */
+    crValues.resize(numTerminals);
+    terminalQueues.resize(numTerminals);
 }
 
 void GroundStation::handleMessage(cMessage *msg)
@@ -21,15 +32,21 @@ void GroundStation::handleMessage(cMessage *msg)
     }
     else
     {
+        /*
+         * TODO: Choose how to make the terminal send its terminal ID to the GS
+         * here I'm assigning it randomly just for testing purposes
+         */
+        crValues[intuniform(0, numTerminals)] = std::stoi(msg->getName());
         acknowledgedCodingRates++;
 
         EV_DEBUG << "[groundStation]> Acknowledged coding rate " << codingRateToString[std::stoi(msg->getName())]
-                << " for terminal "<< "X" << ", "
-                << numTerminals - acknowledgedCodingRates << " remaining" << endl;
+                 << " for terminal "<< "X" << ", "
+                 << numTerminals - acknowledgedCodingRates << " remaining" << endl;
 
         if (acknowledgedCodingRates == numTerminals)
         {
             acknowledgedCodingRates = 0;
+            scheduleTerminals();
             cMessage *frame = new cMessage("frame");
             sendDirect(frame, satellite, "in");
 
@@ -40,6 +57,45 @@ void GroundStation::handleMessage(cMessage *msg)
     }
 
 
+}
+
+bool GroundStation::maxCRPolicy(const TerminalInfo &a, const TerminalInfo &b)
+{
+    return a.crValue > b.crValue;
+}
+
+void GroundStation::scheduleTerminals()
+{
+    /*
+     * Creating a list of terminals with their IDs, CR values, and queues
+     * Just a "fancy" to group TerminalInfos together, not strictly necessary, an array of integers would
+     */
+    std::vector<TerminalInfo> terminals;
+    for (int i = 0; i < numTerminals; ++i) {
+        terminals.push_back({i, crValues[i], terminalQueues[i]});
+    }
+
+    /* Custom comparator to apply maxCRPolicy */
+    std::sort(terminals.begin(), terminals.end(), maxCRPolicy);
+
+    EV_DEBUG << "[groundStation]> Scheduling terminals based on CR values" << endl;
+    buildFrame(terminals);
+}
+
+void GroundStation::buildFrame(std::vector<TerminalInfo>& terminals)
+{
+    for (TerminalInfo &terminal : terminals) {
+        int crValue = terminal.crValue;
+        cQueue &queue = terminal.queue;
+
+        EV_DEBUG << "[groundStation]> Adding Terminal ID=" << terminal.id
+                 << " with CR=" << crValue
+                 << " to frame. Queue size=" << queue.getLength() << endl;
+
+        /*
+        * TODO: build and send out the frame using &queue and crValue
+        */
+    }
 }
 
 void GroundStation::finish()
--- omnetpp/satellite-communications/src/GroundStation.h
+++ omnetpp/satellite-communications/src/GroundStation.h
@@ -6,15 +6,27 @@
 
 using namespace omnetpp;
 
+struct TerminalInfo {
+    int id;
+    int crValue;
+    cQueue queue;
+};
+
 class GroundStation : public cSimpleModule
 {
     int numTerminals;
     int acknowledgedCodingRates;
+    std::vector<int> crValues;
+    std::vector<cQueue> terminalQueues;
     cModule *satellite;
+
 protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
     virtual void finish() override;
+    static bool maxCRPolicy(const TerminalInfo &a, const TerminalInfo &b);
+    void buildFrame(std::vector<TerminalInfo>& terminals);
+    void scheduleTerminals();
 };
 
 #endif /* GROUNDSTATION_H_ */
